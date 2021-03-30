538e0cf909f43a68d71ff9903d40cd2f32bb1fdd
Yan Santana <santanamarianne.v@gmail.com>
2021-03-30 22:03:11 +0800

    Email working. No AJAX

--- forms/contact.php
+++ forms/contact.php
@@ -1,23 +1,22 @@
 <?php
+if(isset($_POST['submit'])){
+  $emailto = 'booking@bluefileimmigration.com';
+  $toname = 'BlueFile';
+  $emailfrom = $_POST["email"];
+  $fromname = $_POST["name"];
+  $subject = $_POST['subject'];
+  $messagebody = "<b>" . $subject . "</b><br>" . $_POST['message'];;
+  $headers = 
+  'Return-Path: ' . $emailfrom . "\r\n" . 
+  'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" . 
+  'X-Priority: 3' . "\r\n" . 
+  'X-Mailer: PHP ' . phpversion() .  "\r\n" . 
+  'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+  'MIME-Version: 1.0' . "\r\n" . 
+  'Content-Transfer-Encoding: 8bit' . "\r\n" . 
+  'Content-Type: text/html; charset=UTF-8' . "\r\n";
+  $params = '-f ' . $emailfrom;
+  $test = mail($emailto, $subject, $messagebody, $headers, $params);
 
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
-
-// Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'santanamarianne.v@gmail.com';
-
-
-$to      = $receiving_email_address;
-$subject =  $_POST['subject'];
-$message = $_POST['message'];
-$headers = array(
-  'From' => $_POST['email'],
-  'Reply-To' => $_POST['email'],
-  'X-Mailer' => 'PHP/' . phpversion()
-);
-
-mail($to, $subject, $message, $headers);
+  echo ($test) ? 'OK' : 'error';
+}
