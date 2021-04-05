d3f520aa264c1af9e101ab1cde9b2f766e80ca56
Yan Santana <santanamarianne.v@gmail.com>
2021-04-05 19:05:07 +0800

    changed receiving email

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-  $emailto = 'booking@bluefileimmigration.com';
+  $emailto = 'bluefile.canada@gmail.com';
   $toname = 'BlueFile';
   $emailfrom = $_POST["email"];
   $fromname = $_POST["name"];
